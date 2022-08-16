86ad313a39c3621c746b281a6eca78d95b2d381f
hoge1e3 <hoge1e3@gmail.com>
2022-08-16 15:15:23 +0900

    somewhat

--- js/zfmExpr.js
+++ js/zfmExpr.js
@@ -17,7 +17,7 @@ define(function (require, exports, module) {
         const funcobjs={
             s: t=>Math.sin(t*Math.PI*2),
             c: t=>Math.cos(t*Math.PI*2),
-            q: t=>(t=>(t<0.5?-1:1))(t%1),
+            q: t=>((t,d=0.5)=>(t<d?-1:1))(t%1),
             r: t=>(t=>(t<0.25? t*4: t<0.75 ? 1-(t-0.25)*4 : -1+(t-0.75)*4))(t%1) ,
             w: t=>(t=>(t<0.5?t*2 : -1+(t-0.5)*2))(t%1),
             d: (r,t)=>(t=>( t<r ? (t/r)*0.5 : 0.5+((t-r)/(1-r))*0.5 ))(t%1),
